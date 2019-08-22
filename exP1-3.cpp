ef85be00fcc8c1bb35b031540724a2b01b8d6555
julesep3 <50899932+julesep3@users.noreply.github.com>
2019-08-22 18:52:06 -0500

    Magic Number 55

---
+++ exP1-3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int sum = 0;
+    for (int i = 1; i < 11; i++)
+    {
+        sum += i;
+    }
+    cout << sum;
+
+    return 0;
+}
