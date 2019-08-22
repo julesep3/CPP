72239cc9a0f342dd22d68039c1dd9d62e9186930
julesep3 <50899932+julesep3@users.noreply.github.com>
2019-08-22 18:46:44 -0500

    Hal 2.0

---
+++ exP1-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, my name is Hal!\n";
+    cout << "What is your name?\n";
+    string user_name;
+    getline(cin,user_name);
+    cout << "Hello, " << user_name << ". I am glad to meet you!";
+
+    return 0;
+}
