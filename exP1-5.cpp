27e4736acaf0b43b8719a4c6efceb73f8e36fdca
julesep3 <50899932+julesep3@users.noreply.github.com>
2019-08-22 19:21:47 -0500

    Name Box

---
+++ exP1-5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << " ________________ \n";
+    cout << "|                |\n";
+    cout << "|     Julian     |\n";
+    cout << "|________________|\n";
+
+    return 0;
+}
